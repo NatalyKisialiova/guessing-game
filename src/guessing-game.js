6d552c2a2f492c748d940589a6350b1367e967f4
ŅatalyKisialiova <nataly.kisialiova@gmail.com>
2024-10-14 11:30:54 +0300

    feat: add solution task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.round(this.min + (this.max - this.min) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.max = this.result;
+        return this.guess();
     }
 
     greater() {
-
+        this.min = this.result;
+        return this.guess(); 
     }
 }
 
